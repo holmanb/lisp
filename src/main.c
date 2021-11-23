018eb7d74447f053e2889789a8886431cc9d483e
Brett Holman <bholman.devel@gmail.com>
2021-11-22 21:37:15 -0700

    add error handling

--- src/main.c
+++ src/main.c
@@ -8,40 +8,107 @@
 
 static char *version = "Lisp Version 0.0.0.0.1";
 
-static long eval_op(long x, char *op, long y)
+enum {
+	LVAL_NUM,
+	LVAL_ERR
+};
+
+enum {
+	LERR_DIV_ZERO,
+	LERR_BAD_OP,
+	LERR_BAD_NUM
+};
+
+struct lval {
+	int type;
+	union {
+		long num;
+		int err;
+	};
+};
+
+struct lval lval_num(long x)
 {
-	if (strcmp(op, "+") == 0)
-		return x + y;
-	if (strcmp(op, "-") == 0)
-		return x - y;
-	if (strcmp(op, "*") == 0)
-		return x * y;
-	if (strcmp(op, "/") == 0)
-		return x / y;
-	return 0;
+	return (struct lval) {
+		.type = LVAL_NUM,
+		.num = x,
+	};
 }
 
-static long eval(mpc_ast_t* t)
+struct lval lval_err(int x)
+{
+	return (struct lval) {
+		.type = LVAL_ERR,
+		.err = x,
+	};
+}
+
+void lval_print(struct lval v){
+	switch (v.type) {
+		case LVAL_NUM:
+			printf("%li", v.num);
+			break;
+		case LVAL_ERR:
+			if (v.err == LERR_DIV_ZERO) {
+				printf("Error: Division By Zero!");
+			} else if (v.err == LERR_BAD_OP) {
+				printf("Error: Invalid Operator!");
+			} else if (v.err == LERR_BAD_NUM) {
+				printf("Error: Invalid Number!");
+			}
+			break;
+		default:
+			printf("Unhandled error!");
+			break;
+	}
+}
+
+void lval_println(struct lval v) { lval_print(v); putchar('\n'); }
+
+static struct lval eval_op(struct lval x, char *op, struct lval y)
+{
+	if (x.type == LVAL_ERR)
+		return x;
+	if (y.type == LVAL_ERR)
+		return y;
+	if (strcmp(op, "+") == 0)
+		return lval_num(x.num + y.num);
+	if (strcmp(op, "-") == 0)
+		return lval_num(x.num - y.num);
+	if (strcmp(op, "*") == 0)
+		return lval_num(x.num * y.num);
+	if (strcmp(op, "/") == 0)
+		return y.num == 0
+			? lval_err(LERR_DIV_ZERO)
+			: lval_num(x.num / y.num);
+	return lval_err(LERR_BAD_OP);
+}
+
+static struct lval eval(mpc_ast_t* t)
 {
 	/* If tagged as number return it directly. */
 	if (strstr(t->tag, "number")) {
-		return atoi(t->contents);
+		errno = 0;
+		long x = strtol(t->contents, NULL, 10);
+		return errno != ERANGE
+			? lval_num(x)
+			: lval_err(LERR_BAD_NUM);
 	}
 
 	/* The operator is always second child. */
 	char* op = t->children[1]->contents;
 
 	/* We store the third child in `x` */
-	long x = eval(t->children[2]);
+	struct lval y = eval(t->children[2]);
 
 	/* Iterate the remaining children and combining. */
 	int i = 3;
 	while (strstr(t->children[i]->tag, "expr")) {
-		x = eval_op(x, op, eval(t->children[i]));
+		y = eval_op(y, op, eval(t->children[i]));
 		i++;
 	}
 
-	return x;
+	return y;
 }
 
 int main(int argc, char *argv[])
@@ -70,7 +137,8 @@ int main(int argc, char *argv[])
 		char *input = readline("lisp> ");
 		add_history(input);
 		if (mpc_parse("<stdin>", input, Lisp, &r)) {
-			printf("%ld\n", eval(r.output));
+			struct lval result = eval(r.output);
+			lval_println(result);
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
